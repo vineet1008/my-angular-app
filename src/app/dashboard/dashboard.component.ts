cf7af1f1c2423fc0ce1dc11af47d8c3a905d5b1e
Vineet <guptavineet287@gmail.com>
2026-04-03 22:36:57 +0530

    Initial commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatTableModule,
+    MatInputModule
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  displayedColumns: string[] = ['name', 'role', 'status'];
+
+  users = [
+    { name: 'Vineet Gupta', role: 'Java Developer', status: 'Active' },
+    { name: 'Rahul Sharma', role: 'Angular Developer', status: 'Pending' },
+    { name: 'Priya Verma', role: 'QA Engineer', status: 'Active' },
+    { name: 'Aman Singh', role: 'DevOps Engineer', status: 'Inactive' }
+  ];
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder,private router:Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log('Login Data:', this.loginForm.value);
+      alert('Login Successful! ' + JSON.stringify(this.loginForm.value));
+      // redirect to dashboard
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
