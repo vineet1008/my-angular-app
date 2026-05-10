36449f7d454c9b629d93e0e5f57d6d06a32069ad
Vineet <guptavineet287@gmail.com>
2026-05-10 18:17:28 +0530

    new user added

--- src/app/add-user-dialog/add-user-dialog.component.ts
+++ src/app/add-user-dialog/add-user-dialog.component.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-user-dialog',
@@ -31,20 +32,73 @@ export class AddUserDialogComponent {
 
   constructor(
     private fb: FormBuilder,
-    private dialogRef: MatDialogRef<AddUserDialogComponent>
+    private dialogRef: MatDialogRef<AddUserDialogComponent>,
+    private http:HttpClient
   ) {
     this.userForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       role: ['', Validators.required],
       status: ['Active', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      mobile_number: [''],
+      username:[''],
+      address:['']
     });
   }
 
+  roles: any[] = [];
+  ngOnInit(): void {
+  this.getRoles();
+  }
+
+  getRoles() {
+
+  this.http.get<any[]>(
+    'http://localhost:8080/api/users/roles'
+  ).subscribe({
+
+    next: (response) => {
+
+      console.log(response);
+
+      this.roles = response;
+    },
+
+    error: (error) => {
+
+      console.error(error);
+    }
+  });
+}
+
   onSave() {
     if (this.userForm.valid) {
+      alert(this.userForm.value);
+      console.log(this.userForm.value);
       this.dialogRef.close(this.userForm.value);
+      this.http.post(
+      'http://localhost:8080/api/users',
+      this.userForm.value
+    ).subscribe({
+
+      next: (response) => {
+
+        console.log('User Saved:', response);
+
+        alert('User Saved Successfully');
+
+        this.dialogRef.close(response);
+      },
+
+      error: (error) => {
+
+        console.error(error);
+
+        alert('Failed to save user');
+      }
+    });
+
     } else {
       this.userForm.markAllAsTouched();
     }
